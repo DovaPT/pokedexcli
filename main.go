8eee83d4ff754de6c34885f07afa5d03d913d8e7
DovaPT <peterd5590@gmail.com>
2025-09-11 17:23:45 +0200

    simple REPL flow

--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main(){
-	var actual, expected string
-	expected = "test"
-
-	if actual != expected {
-		println("Success")
-	} else {
-		println("Failed")
+	inputScanner := bufio.NewScanner(os.Stdin)
+	for ; ; {
+		fmt.Print("pokedex > ")
+		inputScanner.Scan()
+		input := inputScanner.Text()
+		command := cleanInput(input)[0]
+		fmt.Printf("Your command was: %s\n", command)
 	}
 }
 
 func cleanInput(text string) []string{
 	words := strings.Fields(text)
-	fmt.Printf("%v\n", words)
 	return words
 }
